46a38832066b562a7b5ec91c30da3a0955ba5080
yuanhanggao <aaa5131635@163.com>
2022-03-10 20:15:56 +0800

    add threadpool

---
+++ threadpool.cc
@@ -0,0 +1,35 @@
+#include "threadpool.h"
+
+Thread_pool::Thread_pool(int min_thread_num, 
+                         int max_thread_num, int max_queue_size){
+    min_thr_num = min_thread_num;
+    max_thr_num = max_thread_num;
+    busy_thr_num = 0;
+    live_thr_num = min_thread_num;
+    wait_exit_thr_num = 0;
+    queue_front = 0;
+    queue_rear = 0;
+    queue_size = 0;
+    queue_max_size = max_queue_size;
+    shutdown = false;
+
+    threads = new pthread_t[max_thr_num]();
+    if (threads == NULL){
+        printf("new threads false!\n");
+        exit(1);
+    }
+
+    task_queue = new task_t[queue_max_size];
+    if (task_queue == NULL){
+        printf("new task_queue false!\n");
+        exit(1);
+    }
+
+    if (pthread_mutex_init(&lock, NULL) != 0 ||
+        pthread_mutex_init(&thread_counter, NULL) != 0) ||
+        pthread_cond_init(&queue_not_empty, NULL) != 0) ||
+        pthread_cond_init(&queue_not_full, NULL) != 0) {
+        printf("init lock or cond false, due to %d\n", errno);
+        exit(1);
+    }
+}
---
+++ threadpool.h
@@ -0,0 +1,43 @@
+#ifndef THREADPOOL_H_
+#define THREADPOOL_H_
+#include "common.h"
+typedef struct {
+    void *(*function)(void *);
+    void *arg;
+} task_t;
+class Thread_pool {
+private:
+    pthread_mutex_t lock;            // Lock the whole object  
+    pthread_mutex_t thread_counter;  // 
+    pthread_cond_t queue_not_full;
+    pthread_cond_t queue_not_empty;
+
+    pthread_t *threads;              // Vector of pids
+    pthread_t admin_tid;             
+    task_t *task_queue;    
+    int min_thr_num;                 // Minimum number of threads
+    int max_thr_num;                 // Maximum number of threads
+    int live_thr_num;
+    int busy_thr_num;
+    int wait_exit_thr_num;           // Need to be destroyed
+
+    int queue_front;
+    int queue_rear;
+    int queue_size;
+
+    int queue_max_size;
+    int shutdown;                    // state
+
+    int Thread_pool::Free();
+    void *Thread_pool::Admin_thread();
+    void *Thread_pool::Work_thread();
+    
+public:
+    Thread_pool::Thread_pool(int min_thread_num, 
+                             int max_thread_num, int max_queue_size);
+    Thread_pool::~Thread_pool();
+    bool Thread_pool::Is_thread_alive();
+    int Thread_pool::Add_task(void *(*function)(void *arg), void *arg);
+
+};
+#endif
